d995d79ca6fce19cfd7821652bc4b980c56d1ab3
Grayson Gullion <xinflux@gmail.com>
2019-10-26 03:20:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#TODO
+Make a **README.md**
---
+++ src/main/java/blockreminder/BlockReminder.java
@@ -0,0 +1,25 @@
+package blockreminder;
+
+import basemod.BaseMod;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SpireInitializer
+public class BlockReminder {
+
+    BlockReminder() {
+    }
+
+    @SuppressWarnings("unused")
+    public static void initialize() {
+        log("Version", "0.0.0");
+        new BlockReminder();
+    }
+
+    private static void log(String ... items) {
+        System.out.print("RelicSorter ");
+        for(String item : items) {
+            System.out.print(" : " + item);
+        }
+        System.out.println();
+    }
+}
